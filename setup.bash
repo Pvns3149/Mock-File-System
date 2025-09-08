ea328d566ae5c18d02e45555d6d8bae2f60e4dda
Pvns3149 <puvannesan@gmail.com>
2025-09-08 18:44:41 +1000

    bash scripting

---
+++ setup.bash
@@ -0,0 +1,13 @@
+#Add a bash alias for the A1_access.py script and map to the FileSystem command
+echo "alias FileSystem='~/A1_access.py'" >> ~/.bashrc
+
+#Reload the bash file to apply changes
+source ~/.bashrc
+
+#for both files to reformat to allow #! to run on linux
+sed -i 's/\r$//' A1_access.py
+sed -i 's/\r$//' A1_init.py
+
+#Make both files executable
+chmod +x A1_init.py
+chmod +x A1_access.py
